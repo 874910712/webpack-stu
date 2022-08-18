ffef5af84c2122a291cd74fc0d3feaaca52967fa
chengui <874910712@qq.com>
2022-08-18 23:36:53 +0800

    引入corejs彻底解决js兼容性问题（主要针对es6以后产生的相关语法）

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,13 @@ module.exports = {
   // 预设
   presets: [
     //一个智能预设，允许你使用最新的javaScript(es)
-    "@babel/preset-env",
+    ["@babel/preset-env",
+      //corejs按需加载自动引入配置,项目内有用到corejs时才使用
+      {
+        useBuiltIns: "usage",
+        // corejs的版本
+        corejs: 3
+      }]
     // 用于编译React jsx语法的预设
     //'@babel/preset-react',
     // 用于编译TypeScript的预设
--- src/main.js
+++ src/main.js
@@ -8,7 +8,11 @@ import './sass/index.scss'
 // 引入图标文件
 import './css/iconfont.css'
 import './js/iconfont.js'
-
+// ------------------------ core.js用于自动解决js兼容性问题
+// 全部手动引入
+// import 'core-js'
+// 按需加载(以promise为例)
+// import 'core-js/es/promise'
 // ==========================================================
 // if (module.hot) {
 //   // 判断模块是否支持局部热替换
